251180c43331e455945beccedc97bdb419544584
Powers <49866401+DonPangPang@users.noreply.github.com>
2024-03-12 20:40:25 +0800

    The most significant changes involve the addition of new methods and classes to facilitate the creation of fake data. The `System.Linq.Expressions` namespace was also added to the file.
    1. The `System.Linq.Expressions` namespace was added to the file. This namespace provides classes, interfaces, and enumerations that enable language-level code expressions to be represented as objects in the form of expression trees.
    
    2. A new method `FakeAll` was added to the `SnakeskinEntityFrameworkCoreExtensions` class. This method is an extension method for the `ModelBuilder` class and is used for creating fake `User` data.
    
    3. The `Fake<T>` method was modified. It was previously an extension method for the `ModelBuilder` class that returned a `ModelBuilder` instance. Now, it's an extension method that takes an `Action<SnakeskinFakeBuilder<T>>` as a parameter and returns a `SnakeskinEntityFrameworkCoreBuilder` instance.
    
    4. A new method `WithCount` was added to the `SnakeskinEntityFrameworkCoreExtensions` class. This method is an extension method for the `SnakeskinEntityFrameworkCoreBuilder` class and is used for setting the count of fake data.
    
    5. A new class `SnakeskinFakeBuilder<T>` was added. This class has several methods (`Fake<TProperty>`, `FakeName<TProperty>`, `FakePhone<TProperty>`, `FakeValue<TProperty>`, `FakeId<TProperty>`) that return a `SnakeskinFakeBuilder<T>` instance. These methods are used for creating fake data.
    
    6. A new class `User` was added. This class has three properties: `Id`, `Name`, and `Phone`.

--- src/Snakeskin.EntityFrameworkCore/SnakeskinEntityFrameworkCoreExtensions.cs
+++ src/Snakeskin.EntityFrameworkCore/SnakeskinEntityFrameworkCoreExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Snakeskin.EntityFrameworkCore.Core;
+using System.Linq.Expressions;
 
 namespace Snakeskin.EntityFrameworkCore;
 
@@ -9,11 +10,55 @@ public static class SnakeskinEntityFrameworkCoreExtensions
 
     public static ModelBuilder FakeAll(this ModelBuilder modelBuilder)
     {
+        modelBuilder.Fake<User>(x =>
+        {
+            x.FakeId(x => x.Id, true);
+            x.FakeName(x => x.Name);
+            x.FakePhone(x => x.Phone);
+        }).WithCount(100);
+
         return modelBuilder;
     }
 
-    public static ModelBuilder Fake<T>(this ModelBuilder modelBuilder)
+    public static SnakeskinEntityFrameworkCoreBuilder Fake<T>(this ModelBuilder modelBuilder, Action<SnakeskinFakeBuilder<T>> exp)
     {
-        return modelBuilder;
+        return _builder;
     }
+
+    public static SnakeskinEntityFrameworkCoreBuilder WithCount(this SnakeskinEntityFrameworkCoreBuilder builder, int count)
+    {
+        return builder;
+    }
+}
+
+public class SnakeskinFakeBuilder<T>
+{
+    public SnakeskinFakeBuilder<T> Fake<TProperty>(Expression<Func<T, TProperty>> expression)
+    {
+        return this;
+    }
+    public SnakeskinFakeBuilder<T> FakeName<TProperty>(Expression<Func<T, TProperty>> expression)
+    {
+        return this;
+    }
+    public SnakeskinFakeBuilder<T> FakePhone<TProperty>(Expression<Func<T, TProperty>> expression)
+    {
+        return this;
+    }
+    public SnakeskinFakeBuilder<T> FakeValue<TProperty>(Expression<Func<T, TProperty>> expression, TProperty value)
+    {
+        return this;
+    }
+
+    public SnakeskinFakeBuilder<T> FakeId<TProperty>(Expression<Func<T, TProperty>> expression, bool ordered = true)
+    {
+        return this;
+    }
+}
+
+public class User
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Phone { get; set; }
 }
